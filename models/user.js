5c7d02d040905fe03411a36c0cf6908e509dd451
Ibrokhimjon Saydakhmatov <ibrohim95.tashkent@gmail.com>
2018-01-12 16:38:41 +0500

    User model set up

---
+++ models/user.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const validator = require('validator');
+var UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        required: true,
+        trim: true,
+        minlength: 1,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7
+    },
+    profile: {
+        name: {
+            type: String,
+            default: 'User'
+        },
+        picture: {
+            type: String,
+            default: ''
+        },
+        address: String,
+        history: [{
+            date: Date,
+            paid:{type: Number, default: 0},
+            item: {type: mongoose.Schema.Types.ObjectId, ref: ''}
+        }]
+    }
+});
+
+UserSchema.pre('save', (next)=>{
+    var user = this;
+    if(!user.isModified('password')) return next();
+    bcrypt.genSalt(10, (err, salt)=>{
+        if(err) return next(err);
+        bcrypt.hash(user.password, salt, null,(err, hash)=>{
+            if(err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = (password)=>{
+    return bcrypt.compareSync(password, this.password);
+}
+
+var User = mongoose.model('User', UserSchema);
+module.exports = {
+    User
+};
--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const {User} = require('./models/user');
 var app = express();
 
+mongoose.connect('mongodb://localhost:27017/iCommerce', {useMongoClient: true});
+
+app.get('/test', (req,res)=>{
+    var user = new User({
+        email: 'ibrohim@emai.com',
+        password: '1ogizon10'
+    });
+    user.save().then((doc)=>{
+        res.send(doc);
+    }, (e)=>{
+        res.status(400).send(e);
+    });
+    
+});
+app.use(morgan('dev'));
+app.get('/', (req, res)=>{
+    var obj = {name: 'ibrokhim', age: 22, married: false};
+    res.json(obj);
+});
 app.listen(3000, (err)=>{
     if(err) throw err;
     console.log('Server is running');
