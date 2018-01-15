1bc3a6e09f88b45d4fb24c45fe01eac9fee954d8
Ibrokhimjon Saydakhmatov <ibrohim95.tashkent@gmail.com>
2018-01-15 11:39:11 +0500

    Product routes api new views

--- models/product.js
+++ models/product.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
+const mongoosastic = require('mongoosastic');
 var Schema = mongoose.Schema;
 var ProductSchema = new Schema({
     category: {
-        type: Schema.Types.ObjectId, ref: 'Category'
+        type: Schema.Types.ObjectId, 
+        ref: 'Category'
     },
     name: String,
     price: Number,
     image: String
 });
-
+ProductSchema.plugin(mongoosastic, {
+    hosts:[
+        'localhost: 9200'
+    ]
+});
 var Product = mongoose.model('Product', ProductSchema);
 module.exports = {
     Product
--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const {Product} = require('../models/product');
 var indexRouter = express.Router();
-
+Product.createMapping(function(err, mapping){
+    if(err){
+        console.log('error creating mapping');
+        console.log(err);
+    }else{
+        console.log("mapping created");
+        console.log(mapping);
+    }
+});
+var stream = Product.synchronize();
+var count = 0;
+stream.on('data', function(){
+    count++;
+});
+stream.on('close', function(){
+    console.log("Indexed " + count + " documents");
+});
+stream.on('error', function(error){
+    console.error(error);
+    
+});
 indexRouter.get('/', function(req, res) {
     res.render('main/home');
 });
